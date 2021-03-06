f1c063a08cfb2d439b0e78b7407a6c0cadae066c
$udiSimba <sudisimbadav@gmail.com>
2021-03-06 10:58:55 +0200

    Added React-Router

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './app/layout/styles.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './app/layout/styles.css'
 import 'semantic-ui-css/semantic.min.css'
-import App from './app/layout/App';
-import reportWebVitals from './reportWebVitals';
-import { store, StoreContext } from './app/stores/store';
+import App from './app/layout/App'
+import reportWebVitals from './reportWebVitals'
+import { store, StoreContext } from './app/stores/store'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-    <StoreContext.Provider value={store}>
+  <StoreContext.Provider value={store}>
+    <BrowserRouter>
       <App />
-    </StoreContext.Provider>,
+    </BrowserRouter>
+  </StoreContext.Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
