83821e4c78d33b31dbccab7a1b771946d56903a8
$udiSimba <sudisimbadav@gmail.com>
2021-03-07 22:55:22 +0200

    implemted React Router

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,28 +1,29 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid } from 'semantic-ui-react'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 import { useStore } from '../../../app/stores/store'
-import ActivityDetails from '../details/ActivityDetails'
-import ActivityForm from '../form/ActivityForm'
 import ActivityList from './ActivityList'
 
-
 export default observer(function ActivityDashboard() {
+  const { activityStore } = useStore()
+  const { activityRegistry, loadingActivities } = activityStore
 
-        const {activityStore} = useStore();
-        const {selectedActivity, editMode} = activityStore;
+  useEffect(() => {
+    if (activityRegistry.size <= 1) loadingActivities()
+  }, [activityRegistry.size, loadingActivities])
 
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <ActivityList/>
-            </Grid.Column>
-            <Grid.Column width={6}>
-                {selectedActivity && !editMode &&
-                <ActivityDetails />}
-                {editMode &&
-                <ActivityForm />}
-            </Grid.Column>
-        </Grid> 
-    )
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content='Loading App' />
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <h2>Activity Filter!</h2>
+      </Grid.Column>
+    </Grid>
+  )
 })
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,44 +1,36 @@
-import React from 'react'
+import { observer } from 'mobx-react-lite'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
 import { Button, Card, Image } from 'semantic-ui-react'
-import LoadingComponent from '../../../app/layout/LoadingComponent';
-import { useStore } from '../../../app/stores/store';
+import LoadingComponent from '../../../app/layout/LoadingComponent'
+import { useStore } from '../../../app/stores/store'
 
+export default observer(function ActivityDetails() {
+  const { activityStore } = useStore()
+  const { selectedActivity: activity, loadActivity, loadingInitial } = activityStore
+  const {id} = useParams<{id: string}>();
 
-export default function ActivityDetails() {
+  useEffect(() =>{
+    if(id) loadActivity(id)
+  }, [id, loadActivity])
 
-    const {activityStore} = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+  if (loadingInitial || !activity) return <LoadingComponent />
 
-    if(!activity) return <LoadingComponent/>;
-
-    return (
-        <Card fluid>
-            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
-            <Card.Content>
-                    <Card.Header>{activity.title}</Card.Header>
-                    <Card.Meta>
-                        {activity.date}
-                    </Card.Meta>
-                    <Card.Description>
-                        {activity.description}
-                    </Card.Description>
-                </Card.Content>
-            <Card.Content extra>
-                <Button.Group widths={2}>
-                    <Button 
-                        onClick={() => openForm(activity.id)}
-                        basic 
-                        content="Edit" 
-                        color="black"/>
-                    <Button 
-                         onClick={cancelSelectedActivity}
-                        basic 
-                        content="Cancel" 
-                        color="red"/>
-                </Button.Group>
-            </Card.Content>
+  return (
+    <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>{activity.date}</Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths={2}>
+          <Button as={Link} to={`/manage/${activity.id}`} basic content='Edit' color='black' />
+          <Button as={Link} to='/activities' basic content='Cancel' color='red' />
+        </Button.Group>
+      </Card.Content>
     </Card>
-    )
-}
-
- 
+  )
+})
