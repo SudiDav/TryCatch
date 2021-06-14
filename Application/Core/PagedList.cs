6beb641c19e88f2fae762b8cf345e08a13432f14
Sudi Simba <sudisimbadav@gmail.com>
2021-06-14 12:48:27 +0200

    added pagination logic backend

---
+++ Application/Core/PagedList.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Core
+{
+    public class PagedList<T> : List<T>
+    {
+        public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            PageSize = pageSize;
+            TotalCount = count;
+            AddRange(items);
+        }
+
+        public int CurrentPage { get; private set; }
+        public int TotalPages { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }        
+        
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNunber,
+             int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNunber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, count, pageNunber, pageSize);
+        }
+    }
+}
